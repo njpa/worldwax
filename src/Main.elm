5ba968f56971460929dd3d96b2e3c7df2a6d887b
Norman José Pino Añez <normanpino@gmail.com>
2018-12-12 16:55:24 +0100

    Moved to a modular, dev-minded approach to CSS

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,10 @@
 module Main exposing (..)
 
-import Html exposing (text)
+import Html exposing (div, text)
+import Html.Attributes exposing (..)
 
 
 main =
-    text "almost there..."
+    div [ class "w-100 bg-orange vh-100 white f1 tc ttu helvetica" ]
+        [ text "coming soon to a browser near you!"
+        ]
